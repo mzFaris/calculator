eb484c8d90568fd2261ee1106a9c87a097fabc44
Faris <alarifinmirzafaris@gmail.com>
2025-07-05 19:32:03 +0700

    Add calculator UI event listeners and state management

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+const operationVariable = [];
+
+const display = document.querySelector("#display");
+
+const numberContainer = document.querySelector("#number-container");
+numberContainer.addEventListener("click", (e) => {
+  if (e.target.tagName !== "BUTTON") {
+    return;
+  }
+  display.textContent = e.target.id;
+  if (operationVariable.length === 3) {
+    operationVariable.length = 0;
+  }
+  operationVariable.push(+e.target.id);
+  console.log(operationVariable);
+});
+
+const operatorContainer = document.querySelector("#operator-container");
+operatorContainer.addEventListener("click", (e) => {
+  if (e.target.id === "clear") {
+    operationVariable.length = 0;
+    display.textContent = "";
+  } else if (e.target.id === "equal") {
+    operationVariable[0] = operate(
+      operationVariable[0],
+      operationVariable[2],
+      operationVariable[1],
+    );
+    display.textContent = operationVariable[0];
+  } else {
+    if (operationVariable.length === 2) {
+      operationVariable.length = 1;
+    }
+    operationVariable.push(e.target.textContent);
+  }
+  console.log(operationVariable);
+});
+
 function operate(a, b, operator) {
   switch (operator) {
     case "+":
