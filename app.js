a8cdbaed7085725507394a43d72a30fd019e9f78
Faris <alarifinmirzafaris@gmail.com>
2025-07-05 16:37:53 +0700

    Add operate function for basic arithmetic operations

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+function operate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+}
+
 function add(a, b) {
   return a + b;
 }
